8a6780e11bb4a2ad2a60fc874bc8a2d61687feeb
Fedor <tarassovf@gmail.com>
2013-04-24 15:13:04 +0300

    posix module

---
+++ src/posix.cc
@@ -0,0 +1,25 @@
+#include <node.h>
+#include <unistd.h>
+#include <v8.h>
+
+using namespace v8;
+
+Handle<Value> Fork(const Arguments& args) {
+  HandleScope scope;
+  return scope.Close(Integer::New(fork()));
+}
+
+Handle<Value> Daemon(const Arguments& args) {
+  HandleScope scope;
+  return scope.Close(Integer::New(daemon(args[0]->IsTrue() ? 0 : -1,
+                                         args[1]->IsTrue() ? 0 : -1)));
+}
+
+void init(Handle<Object> exports) {
+  exports->Set(String::NewSymbol("fork"),
+      FunctionTemplate::New(Fork)->GetFunction());
+  exports->Set(String::NewSymbol("daemon"),
+      FunctionTemplate::New(Daemon)->GetFunction());
+}
+
+NODE_MODULE(posix, init)
